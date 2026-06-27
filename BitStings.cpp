ff98e1670e79e425339622e35c3dd134be4189e0
ishant-fn <ishantbaban@gmail.com>
2026-06-27 12:22:14 +0300

    BitStings.cpp
    Added a comment to clarify the purpose of the modulo operation.

--- BitStings.cpp
+++ BitStings.cpp
@@ -9,7 +9,7 @@ int main()
     
     while (n > 1) {
         answer *= 2;
-        answer %= 1000000007;
+        answer %= 1000000007; // This is used to stop long longs from overflowing. The exact value will be given by CSES.
         --n;
     }
     
