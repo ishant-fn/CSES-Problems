86f82d03ef5642ffeeaf89199bb1dd20daec6e31
ishant-fn <ishantbaban@gmail.com>
2026-06-28 14:41:54 +0300

    WeirdAlgorithm.cpp

---
+++ WeirdAlgorithm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n = 0;
+    cin >> n;
+    
+    while (n > 1) {
+        cout << n << " ";
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n *= 3;
+            ++n;
+        }
+    }
+    cout << n << " ";
+    
+    return 0;
+}
