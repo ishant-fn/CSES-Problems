b2adcdde3a8cf9de8f2a680558080269b8c51aa5
ishant-fn <ishantbaban@gmail.com>
2026-07-04 21:32:42 +0300

    Nopat(Datatähti-question).cpp

--- Nopat(Datatähti-question).cpp
+++ Nopat(Datatähti-question).cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    cout << (1 << n) / 2;
+    
+    return 0;
+}
